f4212649fbb17e264a7878e6a3cf8e4d89ecbb39
Dev-Osmium <oatmas64134@gmail.com>
2018-04-20 19:16:56 -0400

    Finish up

--- bot.js
+++ bot.js
@@ -1,14 +1,14 @@
 const discord = require('discord.js');
-const client = new Discord.Client();
+const client = new discord.Client();
 const winston = require('winston');
-const config = require('config.json');
+const config = require('./config.json');
 
 var logger = new winston.Logger({
     transports: [
         new winston.transports.File({
             level: 'info',
             filename: 'messageLog.log',
-            json: true,
+            json: false,
             colorize: true
         }),
         new winston.transports.Console({
@@ -22,13 +22,17 @@ var logger = new winston.Logger({
 
 client.on('ready', () => {
     logger.log('info', 'Initialized Successfully');
-    client.setGame('with the logs!');
+    client.user.setActivity('with the logs!');
 })
 
 client.on('message', (message) => {
+    if (message.author.bot) return;
     logger.log('info', `${message.author.username} sent a message containing the following text: ${message.content}`);
     if(config.logChannel) {
-        logChannel = message.guild.channels.get(config.logChannelId);
+        guild = message.guild;
+        logChannel = guild.channels.get(config.logChannelId);
         logChannel.send(`${message.author.username} sent a message containing the following text: ${message.content}`)
     }
 })
+
+client.login(config.token);
