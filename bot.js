22e696abca6b95713b540d7a29c283276b4a98f9
Owen Salter <owen@hephaestus.localdomain>
2020-04-01 08:27:19 -0400

    feat(log): Channel logs use embeds
    No more plaintext for channels, those are just for file logs

--- bot.js
+++ bot.js
@@ -27,6 +27,13 @@ client.on('ready', () => {
 
 client.on('message', (message) => {
     var logMsg = `${message.author.username}#${message.author.discriminator} sent a message in <#${message.channel.id}>: ${message.content}`;
+    var logEmbed = new discord.RichEmbed()
+        .setColor('#00007f')
+        .setAuthor(message.author.username + '#' + message.author.discriminator, 'https://no.domain')
+        .setTitle("Message Log")
+        .addField("Channel", message.channel.name)
+        .addField("Author", message.author.username)
+        .setDescription(message.content);
     if (message.guild) {
         if (message.author.bot) return;
         logger.log('info', logMsg);
@@ -37,7 +44,7 @@ client.on('message', (message) => {
         if (config.logChannel) {
             guild = message.guild;
             logChannel = guild.channels.get(config.logChannelId);
-            logChannel.send(logMsg);
+            logChannel.send(logEmbed);
         }
     }
 })
